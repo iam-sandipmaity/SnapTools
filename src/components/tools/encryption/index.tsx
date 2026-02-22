4c2c9797f14008926a2cdbe3a5da593bdab18ea2
Sandip Maity <social.sandipmaity@gmail.com>
2026-02-23 01:44:10 +0530

    Register new modern encryption subtools and polyfill-backed legacy tools.

--- src/components/tools/encryption/index.tsx
+++ src/components/tools/encryption/index.tsx
@@ -13,7 +13,6 @@ import MD5Tool from './MD5Tool';
 import MD4Tool from './MD4Tool';
 import MD2Tool from './MD2Tool';
 import MDC2Tool from './MDC2Tool';
-import MCDTool from './MCDTool';
 import RIPEMD160Tool from './RIPEMD160Tool';
 import SHATool from './SHATool';
 import SHA3Tool from './SHA3Tool';
@@ -21,6 +20,16 @@ import URLEncodeTool from './URLEncoderTool';
 import BCryptTool from './BCryptTool';
 import SCryptTool from './SCryptTool';
 import PBKDF2Tool from './PBKDF2Tool';
+import Chacha20Poly1305Tool from './Chacha20Poly1305Tool';
+import XChaCha20Tool from './XChaCha20Tool';
+import AESGCMTool from './AESGCMTool';
+import ECDHTool from './ECDHTool';
+import X25519Tool from './X25519Tool';
+import Ed25519Tool from './Ed25519Tool';
+import ECDSATool from './ECDSATool';
+import Blake2Tool from './Blake2Tool';
+import Blake3Tool from './Blake3Tool';
+import Argon2Tool from './Argon2Tool';
 
 const encryptionTools: EncryptionToolComponentMap = {
   'aes': AESTool,
@@ -36,7 +45,6 @@ const encryptionTools: EncryptionToolComponentMap = {
   'md4': MD4Tool,
   'md2': MD2Tool, // 'md2': 'MD2'
   'mdc2': MDC2Tool, //'mdc2': 'MDC2'
-  'mcd': MCDTool, //'mcd': 'MCD'
   'ripemd160': RIPEMD160Tool,
   'sha': SHATool,
   'sha3': SHA3Tool,
@@ -44,7 +52,19 @@ const encryptionTools: EncryptionToolComponentMap = {
   'bcrypt': BCryptTool,
   'scrypt': SCryptTool,
 
-  'pbkdf2': PBKDF2Tool
+  'pbkdf2': PBKDF2Tool,
+
+  // modern placeholders
+  'chacha20_poly1305': Chacha20Poly1305Tool,
+  'xchacha20': XChaCha20Tool,
+  'aes_gcm': AESGCMTool,
+  'ecdh': ECDHTool,
+  'x25519': X25519Tool,
+  'ed25519': Ed25519Tool,
+  'ecdsa': ECDSATool,
+  'blake2': Blake2Tool,
+  'blake3': Blake3Tool,
+  'argon2': Argon2Tool
 };
 
 
